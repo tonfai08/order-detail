3e1721f24c24993917c2c670d166b823554b4e73
tonfai08 <tonbee11@email.com>
2025-04-11 11:43:19 +0700

    update

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -3,9 +3,13 @@ import axios from "axios";
 const API_URL = "https://haikyu-be.vercel.app/api/floating";
 
 // POST: ส่งข้อความใหม่
-export const sendFloatingMessage = async (name: string, detail: string) => {
+export const sendFloatingMessage = async (
+  name: string,
+  detail: string,
+  token: string
+) => {
   try {
-    const response = await axios.post(API_URL, { name, detail });
+    const response = await axios.post(API_URL, { name, detail, token });
     return response.data;
   } catch (error) {
     console.error("❌ Error sending message:", error);
